4b815524e095f6227f9af3c2b937b5b85936cd21
xafixav <xafiworld@gmail.com>
2021-09-29 11:06:54 -0300

    requirement 1 again, fixed

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,8 +10,8 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('1 - O retorno do telefonema', () => {
-  test('atende', async () => {
-    await answerPhone(true).then((result) => result.toEqual('Oi!'));
+  test('atende', () => {
+    answerPhone(true).then((result) => result.toEqual('Oi!'));
     // Insira seu teste assíncrono aqui
   });
   test('ocupado', () => {
